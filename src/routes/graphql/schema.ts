88c4b8a78c7325f35c946541437470019eaf9b5d
firespark <fireleit@gmail.com>
2024-10-27 16:19:53 +0400

    feat: add inputs to schema.ts

--- src/routes/graphql/schema.ts
+++ src/routes/graphql/schema.ts
@@ -6,10 +6,13 @@ import {
     GraphQLInt,
     GraphQLEnumType,
     GraphQLFloat,
-    GraphQLList
+    GraphQLList,
+    GraphQLInputObjectType
 } from 'graphql';
 import { UUIDType } from './types/uuid.js';
 import { MemberTypeId } from '../member-types/schemas.js';
+
+// Types
 
 const MemberTypeIdEnum = new GraphQLEnumType({
     name: 'MemberTypeId',
@@ -58,4 +61,59 @@ const UserType = new GraphQLObjectType({
         userSubscribedTo: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))) },
         subscribedToUser: { type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(UserType))) },
     }),
+});
+
+
+// Input Types
+
+const ChangePostInputType = new GraphQLInputObjectType({
+    name: 'ChangePostInput',
+    fields: {
+        title: { type: GraphQLString },
+        content: { type: GraphQLString },
+    },
+});
+
+const ChangeProfileInputType = new GraphQLInputObjectType({
+    name: 'ChangeProfileInput',
+    fields: {
+        isMale: { type: GraphQLBoolean },
+        yearOfBirth: { type: GraphQLInt },
+        memberTypeId: { type: MemberTypeIdEnum },
+    },
+});
+
+const ChangeUserInputType = new GraphQLInputObjectType({
+    name: 'ChangeUserInput',
+    fields: {
+        name: { type: GraphQLString },
+        balance: { type: GraphQLFloat },
+    },
+});
+
+const CreatePostInputType = new GraphQLInputObjectType({
+    name: 'CreatePostInput',
+    fields: {
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        content: { type: new GraphQLNonNull(GraphQLString) },
+        authorId: { type: new GraphQLNonNull(UUIDType) },
+    },
+});
+
+const CreateProfileInputType = new GraphQLInputObjectType({
+    name: 'CreateProfileInput',
+    fields: {
+        isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+        yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+        userId: { type: new GraphQLNonNull(UUIDType) },
+        memberTypeId: { type: new GraphQLNonNull(MemberTypeIdEnum) },
+    },
+});
+
+const CreateUserInputType = new GraphQLInputObjectType({
+    name: 'CreateUserInput',
+    fields: {
+        name: { type: new GraphQLNonNull(GraphQLString) },
+        balance: { type: new GraphQLNonNull(GraphQLFloat) },
+    },
 });
